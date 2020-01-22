d98bf1ac15ee8281335708a9de10ef937d3d54c9
abekedevelop <abekedevelop@gmail.com>
2020-01-22 22:49:01 +0600

    add process contract

---
+++ app/Domain/Contracts/ProcessContract.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abk
+ * Date: 1/22/20
+ * Time: 10:48 PM
+ */
+
+namespace App\Domain\Contracts;
+
+
+interface ProcessContract extends RunnableContract
+{
+
+}
